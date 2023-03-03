06830ace80bfe9f971575990f27010c0da045ec8
Omar <vip.mergeshev@gmail.com>
2023-03-03 22:03:19 +0600

    add homeworks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 
 function App() {
-  const [ names ] = useState(["Арген", "Максат", "Жанылай"])
+  const [names, setNames] = useState(["Арген", "Максат", "Жанылай"])
+
+  const keysNames = names.map(n => {
+    return {
+      name: n,
+      key: (Math.random() + 1).toString(36).substring(7)
+    }
+  })
 
   return (
     <ul>
-      {names.map(name => (
-        <li key={name}>{name}</li>
+      {keysNames.map(({ name, key }) => (
+        <li key={key}>{name}</li>
       ))}
     </ul>
   );
