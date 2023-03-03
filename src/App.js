27bd0d86944d6e66b755e5c51473ae8ad0f877b7
Omar <vip.mergeshev@gmail.com>
2023-03-03 21:35:29 +0600

    add lesson6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [ names ] = useState(["Арген", "Максат", "Жанылай"])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {names.map(name => (
+        <li key={name}>{name}</li>
+      ))}
+    </ul>
   );
 }
 
